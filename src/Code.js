a192cf4de4027eddea92fe18f4b3296582ccf6a3
yono <firebrick26@gmail.com>
2019-07-20 16:19:57 +0900

    Refine code

--- src/Code.js
+++ src/Code.js
@@ -1,53 +1,61 @@
-var SCRIPT_PROPERTY_KEY_DEST_SHEET_ID = 'dest_sheet_id';
+var SCRIPT_PROPERTY_KEY_OUTPUT_SHEET_ID = 'output_sheet_id';
 
-function myFunction() {
-  var scriptProperties = PropertiesService.getScriptProperties();
-  var sheetId = scriptProperties.getProperty(SCRIPT_PROPERTY_KEY_DEST_SHEET_ID);
-
+var sheet = (function () {
   var sheets = SpreadsheetApp.getActive().getSheets();
-  var sheet = sheetId ? sheets.filter(function (s) { return s.getId() === sheetId; }) : sheets[0];
-  if (!sheet) {
-    throw new Error('sheet not found');
+  var sheetId = PropertiesService.getScriptProperties().getProperty(SCRIPT_PROPERTY_KEY_OUTPUT_SHEET_ID);
+  if (sheetId) {
+    return sheets.filter(function (sheet) {
+      return sheet.getId() === sheetId;
+    })[0];
   }
+  return sheets[0];
+}());
+
+var searchMessages = function (query, start, max) {
+  query = query || '';
+  start = start || 0;
+  max = max || 100;
+
+  return GmailApp.search(query, start, max).reverse().map(function (gmailThread) {
+    return gmailThread.getMessages()[0];
+  });
+};
+
+var notify = function () {};
+
+function execute() {
+  if (sheet == null) throw new Error('Error: sheet not found');
 
   var query = [
     'in:inbox',
-    'is:unread',
-    'label:example'
+    'is:unread'
+//    'label:example'
   ].join(' ');
 
-  Logger.log('Query: "' + query + '"');
+  var notifyNeeded = false;
 
-  var isDirty = false;
+  var gmailMessages = searchMessages(query);
+  Logger.log('Messages: ' + gmailMessages.length);
+  for (var i=0; i<gmailMessages.length; i++) {
+    var gmailMessage = gmailMessages[i];
+    Logger.log('Message: ' + gmailMessage.getId());
 
-  var gmailThreads = GmailApp.search(query);  // TODO: Consider start, max
-  Logger.log('Results: ' + gmailThreads.length);
+    var date = gmailMessage.getDate();
+    var from = gmailMessage.getFrom();
+    var plainBody = gmailMessage.getPlainBody();
+    
+    sheet.appendRow([
+      date,
+      from,
+      plainBody
+    ]);
 
-  for (var i=0; i<gmailThreads.length; i++) {
-    Logger.log('Thread ID: ' + gmailThreads[i].getId());
-
-    var gmailMessage = gmailThreads[i].getMessages()[0];
-    if (gmailMessage == null) {
-      Logger.log('\tno first message.');
-      continue;
-    }
-    Logger.log('\tMessage ID: ' + gmailMessage.getId());
-
-    var data = [
-      gmailMessage.getDate(),
-      gmailMessage.getFrom(),
-      gmailMessage.getPlainBody()
-    ];
-    var lastRow = sheet.getLastRow();
-    sheet.getRange(lastRow+1, 1, 1, data.length).setValues([data]);
-
-    gmailThreads[i].markRead();
-
-    isDirty = true;
+    gmailMessage.markRead();
+    notifyNeeded = true;
+    Logger.log('ok');
   }
 
-  if (isDirty) {
-    // TODO: notify changes to a chat service or something else.
-    Logger.log('notify feature is not implemented yet');
+  if (notifyNeeded) {
+    notify();
   }
 }
