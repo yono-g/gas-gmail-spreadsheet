20627a6fc28d939a343ed8c19f8f9aa4b52ad6a6
yono <firebrick26@gmail.com>
2019-07-20 17:20:08 +0900

    Implement chatwork notification

--- src/Code.js
+++ src/Code.js
@@ -1,14 +1,36 @@
-var SCRIPT_PROPERTY_KEY_OUTPUT_SHEET_ID = 'output_sheet_id';
+var SCRIPT_PROPERTY_KEYS = {
+  OUTPUT_SHEET_ID: 'output_sheet_id',
+  NOTIFICATION_MESSAGE: 'notification_message',
+  CHATWORK_API_TOKEN: 'chatwork_api_token',
+  CHATWORK_ROOM_ID: 'chatwork_room_id'
+};
 
 var sheet = (function () {
   var sheets = SpreadsheetApp.getActive().getSheets();
-  var sheetId = PropertiesService.getScriptProperties().getProperty(SCRIPT_PROPERTY_KEY_OUTPUT_SHEET_ID);
+  var sheetId = PropertiesService.getScriptProperties().getProperty(SCRIPT_PROPERTY_KEYS.OUTPUT_SHEET_ID);
   if (sheetId) {
     return sheets.filter(function (sheet) {
       return sheet.getId() === sheetId;
     })[0];
   }
   return sheets[0];
+}());
+
+var notificationSettings = (function () {
+  var scriptProperties = PropertiesService.getScriptProperties();
+  var apiToken = scriptProperties.getProperty(SCRIPT_PROPERTY_KEYS.CHATWORK_API_TOKEN);
+  var roomId = scriptProperties.getProperty(SCRIPT_PROPERTY_KEYS.CHATWORK_ROOM_ID);
+  var message = scriptProperties.getProperty(SCRIPT_PROPERTY_KEYS.NOTIFICATION_MESSAGE);
+  
+  if (apiToken == null || roomId == null || message == null) {
+    return null;
+  }
+  
+  return {
+    apiToken: apiToken,
+    roomId: roomId,
+    message: message
+  };
 }());
 
 var searchMessages = function (query, start, max) {
@@ -21,21 +43,33 @@ var searchMessages = function (query, start, max) {
   });
 };
 
-var notify = function () {};
+var notify = function () {
+  if (notificationSettings == null) {
+    Logger.log('[Notice] Skip notification due to insufficient settings');
+    return;
+  }
+
+  ChatWorkClient.factory({
+    token: notificationSettings.apiToken
+  }).sendMessage({
+    room_id: notificationSettings.roomId,
+    body: notificationSettings.message
+  });
+};
 
 function execute() {
-  if (sheet == null) throw new Error('Error: sheet not found');
+  if (sheet == null) throw new Error('[Error] sheet not found');
 
   var query = [
     'in:inbox',
     'is:unread'
 //    'label:example'
   ].join(' ');
-
-  var notifyNeeded = false;
-
   var gmailMessages = searchMessages(query);
   Logger.log('Messages: ' + gmailMessages.length);
+
+  var isDirty = false;
+
   for (var i=0; i<gmailMessages.length; i++) {
     var gmailMessage = gmailMessages[i];
     Logger.log('Message: ' + gmailMessage.getId());
@@ -43,7 +77,7 @@ function execute() {
     var date = gmailMessage.getDate();
     var from = gmailMessage.getFrom();
     var plainBody = gmailMessage.getPlainBody();
-    
+
     sheet.appendRow([
       date,
       from,
@@ -51,11 +85,14 @@ function execute() {
     ]);
 
     gmailMessage.markRead();
-    notifyNeeded = true;
+
+    isDirty = true;
     Logger.log('ok');
   }
 
-  if (notifyNeeded) {
+  if (isDirty) {
     notify();
   }
+  
+  Logger.log('done');
 }
